c22740137ceb3d3c85f2c250bbb67c8d2f76edb2
karan bangia <karanbangia14@gmail.com>
2019-12-29 23:21:35 +0530

    added comments

--- Problem_15_3_sum.java
+++ Problem_15_3_sum.java
@@ -1,10 +1,12 @@
   class Solution {
+  
       public List<List<Integer>> threeSum(int[] nums) {
           int n=nums.length;
           Arrays.sort(nums);
           List<List<Integer>> ans=new ArrayList<>();
           for(int i=0;i<n-2;i++)
           {
+            //last ke 3 chhodne hai issliye n-2 limiting haii
               if(i==0 ||(i>0 && nums[i]!=nums[i-1]))
               {
                   int low=i+1;
@@ -12,9 +14,11 @@
                   int sum=-nums[i];
                   while(low<high)
                   {
+                    //two-pointer approach
                       if(nums[low]+nums[high]==sum)
                       {
-                        ans.add(Arrays.asList(nums[i],nums[low],nums[high]));  
+                        ans.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        //to remove next same number  
                         while(low<high && nums[low]==nums[low+1])low++;
                         while(low<high && nums[high]==nums[high-1])high--;
                         low++;
@@ -35,4 +39,3 @@
               }
   }
                   
-        
