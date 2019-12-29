43000f6782fb01960c345313bba2ce6d939afea7
karan bangia <karanbangia14@gmail.com>
2019-12-29 19:34:45 +0530

    optimized solution

--- Problem_11_container_with_most_water.java
+++ Problem_11_container_with_most_water.java
@@ -14,3 +14,27 @@ class Solution {
     }
 }
 //optimized solution
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        int area=0;
+        while(i<j)
+        {
+            area=Math.max(area,Math.min(height[i],height[j])*(j-i));
+            if(height[i]<height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+            
+        }
+        return area;
+        
+    }
+}
+
