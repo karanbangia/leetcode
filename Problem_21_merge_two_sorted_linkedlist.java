d466dedeaa3a4b846e12ac14b256a94017a49419
karan bangia <karanbangia14@gmail.com>
2019-12-30 18:49:29 +0530

    added recursion solution

--- Problem_21_merge_two_sorted_linkedlist.java
+++ Problem_21_merge_two_sorted_linkedlist.java
@@ -6,6 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+//basic solution
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2)
     {
@@ -36,6 +37,33 @@ class Solution {
             ll.next=l2;
         }
        return temp.next;
+    }
+}
+   
+//recursion solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+    {
+       if(l1==null)return l2;
+       if(l2==null)return l1;
+        if(l1.val<=l2.val)
+        {
+            l1.next=mergeTwoLists(l1.next,l2);
+            return l1;
+        }
+        else
+        {
+            l2.next=mergeTwoLists(l1,l2.next);
+            return l2;
+        }
     }
 }
    
