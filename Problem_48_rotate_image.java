3a1cd0c69819064e3962816a8c30460696bf6a5b
karan bangia <karanbangia14@gmail.com>
2020-01-03 01:36:25 +0530

    added code

---
+++ Problem_48_rotate_image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n/2;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-i-1][j];
+                matrix[n-i-1][j]=temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int data = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = data;
+            }
+        }
+    }
+}
