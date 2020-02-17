edbd171a21d02bf9146ac9929151c8036f018ba9
karanbangia <karanbangia14@gmail.com>
2020-02-17 15:45:55 +0530

    added code

---
+++ Problem_122_best_time_to_sell_stocks_2.java
@@ -0,0 +1,23 @@
+//take two variables min price and maxProfit
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        int totalProfit=0;
+        int n=prices.length;
+        for(int i=0;i<n;i++)
+        {
+           if(prices[i]<minPrice)
+           {
+               minPrice=prices[i];
+           }
+            else if(prices[i]-minPrice>maxProfit)
+            {
+                maxProfit=prices[i]-minPrice;
+                totalProfit+=maxProfit;
+                minPrice=Integer.MAX_VALUE;
+            }
+        }
+        return totalProfit;
+    }
+}
---
+++ Problem_746_min_cost_climbing_stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int dp[]=new int[n];
+        dp[n-1]=cost[n-1];
+        dp[n-2]=cost[n-2];
+        for(int i=n-3;i>=0;i--)
+        {
+            dp[i]=Math.min(cost[i]+dp[i+1],cost[i]+dp[i+2]);
+        }
+        return Math.min(dp[0],dp[1]);
+    }
+}
